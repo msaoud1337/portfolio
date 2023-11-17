e0ce5779cdd84e20b2ddf02a0066c0a6f12900c7
msaoud1337 <aminox1998@outlook.fr>
2023-11-17 14:03:06 +0100

    refactor(resume): skills icon animation refactored

--- components/ResumeArticle.tsx
+++ components/ResumeArticle.tsx
@@ -10,7 +10,7 @@ import {
   Typography,
 } from '@mui/material';
 import { AnimatePresence, motion } from 'framer-motion';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import { aboutMeText, internShipText } from '@/utils/texts';
 
@@ -121,19 +121,7 @@ const Cards = ({ text, icon, title }: CardsProps) => {
 };
 
 export default function ResumeArticle() {
-  const [icons, setIcons] = useState<{ href: string; title: string }[]>([]);
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  useEffect(() => {
-    const addIconWithDelay = async () => {
-      for (const icon of array) {
-        await new Promise((resolve) => setTimeout(resolve, 600));
-        const updatedIconState = !icons.includes(icon) ? [...icons, icon] : [...icons];
-        setIcons(updatedIconState);
-      }
-    };
-
-    addIconWithDelay();
-  }, []);
 
   const INTERSTINGS = [
     {
@@ -158,10 +146,10 @@ export default function ResumeArticle() {
       title: 'Mobile Dev',
       icon: (
         <svg xmlns="http://www.w3.org/2000/svg" width="190" height="190" viewBox="0 0 24 24">
-          <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="1.5">
-            <path stroke-linejoin="round" d="m12 19.01l.01-.011" />
+          <g fill="none" stroke="currentColor" strokeLinecap="round" strokeWidth="1.5">
+            <path strokeLinejoin="round" d="m12 19.01l.01-.011" />
             <path d="M18 18v3.4a.6.6 0 0 1-.6.6H6.6a.6.6 0 0 1-.6-.6V18M18 6V2.6a.6.6 0 0 0-.6-.6H6.6a.6.6 0 0 0-.6.6V6" />
-            <path stroke-linejoin="round" d="M15.5 8.5L19 12l-3.5 3.5m-7-7L5 12l3.5 3.5" />
+            <path strokeLinejoin="round" d="M15.5 8.5L19 12l-3.5 3.5m-7-7L5 12l3.5 3.5" />
           </g>
         </svg>
       ),
@@ -232,11 +220,21 @@ export default function ResumeArticle() {
               My Current web dev skills:
             </Typography>
             <Stack direction={'row'} flexWrap={'wrap'} gap={1} pb={2}>
-              {icons.map((icon, id) => (
-                <Tooltip key={id} title={icon.title}>
-                  <motion.img height={40} width={40} {...varFadeInRight} src={icon.href} />
-                </Tooltip>
-              ))}
+              {array.map((icon, id) => {
+                const iconTRansition = {
+                  duration: 0.64 + id / 4,
+                  ease: [0.43, 0.13, 0.23, 0.96],
+                };
+                const iconFadeToRight = {
+                  initial: { x: DISTANCE, opacity: 0 },
+                  animate: { x: 0, opacity: 1, transition: iconTRansition },
+                };
+                return (
+                  <Tooltip key={id} title={icon.title}>
+                    <motion.img height={40} width={40} {...iconFadeToRight} src={icon.href} />
+                  </Tooltip>
+                );
+              })}
             </Stack>
           </Box>
         </Box>

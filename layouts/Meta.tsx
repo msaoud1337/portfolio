62cc5e3d6c3e3eae843ca069d3bdadd6763abbdf
msaoud1337 <aminox1998@outlook.fr>
2024-08-28 22:24:45 +0100

    feat(favicon): add new favicon to project

--- layouts/Meta.tsx
+++ layouts/Meta.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import { useRouter } from 'next/router';
 import { NextSeo } from 'next-seo';
 
 export type IMetaProps = {
@@ -9,30 +8,12 @@ export type IMetaProps = {
 };
 
 const Meta = (props: IMetaProps) => {
-  const router = useRouter();
-
   return (
     <>
       <Head>
         <meta charSet="UTF-8" key="charset" />
         <meta name="viewport" content="width=device-width,initial-scale=1" key="viewport" />
-        <link rel="apple-touch-icon" href={`${router.basePath}/apple-touch-icon.png`} key="apple" />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href={`${router.basePath}/favicon-32x32.png`}
-          key="icon32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href={`${router.basePath}/favicon-16x16.png`}
-          key="icon16"
-        />
-        <link href="https://api.mapbox.com/mapbox-gl-js/v1.10.1/mapbox-gl.css" rel="stylesheet" />
-        <link rel="icon" href={`${router.basePath}/favicon.ico`} key="favicon" />
+        <link rel="icon" href={`faviconT.png`} key="favicon" />
       </Head>
       <NextSeo
         title={props.title}

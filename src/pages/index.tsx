4d48b7fa1944483b17d4c2bd34c99f54f18be472
msaoud1337 <aminox1998@outlook.fr>
2024-09-28 18:31:26 +0100

    style(navBar): reemove padding saffe erea bottom and adding it in height

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -86,9 +86,8 @@ const MobileNav = ({ tabValue }: Props) => {
             padding: 0,
             justifyContent: 'center',
             alignItems: 'center',
-            height: '55px',
+            height: 'calc(55px + env(safe-area-inset-bottom))',
             margin: 0,
-            paddingBottom: 'env(safe-area-inset-bottom)',
             li: {
               position: 'relative',
               display: 'flex',
